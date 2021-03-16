0e313428ebb7831056511d49e11550f2c161825d
Morgan <mlpeck@fullsail.edu>
2021-03-15 21:00:00 -0500

    running prettier

--- src/Components/PlacesSearch.vue
+++ src/Components/PlacesSearch.vue
@@ -2,36 +2,29 @@
   <h1>Weather Forecast</h1>
   <div class="form-group">
     <label for="location">Search for location</label>
-    <input type="text" name="location" id="location">
+    <input type="text" name="location" id="location" />
   </div>
   <state-dropdown />
   <button>Search</button>
 </template>
-
 <script>
-import stateDropdown from '@/Components/StatesDropdown'
+import stateDropdown from '@/Components/StatesDropdown';
 
 export default {
-
-  components : {
-      stateDropdown
+  components: {
+    stateDropdown
   },
-  props: ['states.value'],
-  data(){
-    return{
+  // props: ['states.value'],
+  data() {
+    return {
       apiKey: '4de4a8d80078d333a4a72f1c11d87820',
       url: 'https://api.openweathermap.org/data/2.5/',
       weatherData: [],
       error: null
-    }
+    };
   },
-  methods: {
-   
-  }
-
-}
+  methods: {}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
