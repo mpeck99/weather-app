460b740198dc538b85a376d731ebd7c18d8161e7
Morgan <mlpeck@fullsail.edu>
2021-03-21 14:57:25 -0500

    adding some variables

--- src/Components/CurrentWeather.vue
+++ src/Components/CurrentWeather.vue
@@ -4,8 +4,11 @@
     <div class="header">
       <p>{{ this.day }}</p>
       <p>{{ this.date }}</p>
+      <p>{{ this.time }}</p>
     </div>
     <div class="body">
+      <img :src="this.icon" :alt="this.description" />
+      <p>{{ this.description }}</p>
       <p>{{ this.temp }}</p>
       <p>{{ this.currentWeather }}</p>
     </div>
@@ -19,28 +22,43 @@ export default {
   },
   data() {
     return {
-      day: ' ',
-      date: ' ',
+      day: '',
+      date: '',
       location: '',
       currentWeather: this.data.current,
-      temp: ' '
+      temp: '',
+      icon: '',
+      description: '',
+      time: ''
     };
   },
-  // mounted() {
-  //   this.data += '';
-  // },
   watch: {
     data: {
       immediate: true,
       deep: true,
       handler(weather) {
         if (weather.current != undefined) {
-          this.temp = Math.round(weather.current.temp);
+          this.temp =
+            Math.round(weather.current.temp) + String.fromCharCode(176);
           this.day = new Date().toLocaleDateString('en-us', {
             weekday: 'long'
           });
           this.location = weather.location;
           this.date = new Date().toLocaleDateString();
+          this.icon =
+            'http://openweathermap.org/img/wn/' +
+            weather.current.weather[0].icon +
+            '.png';
+          this.description = weather.current.weather[0].description;
+          const timeOptions = {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+          };
+          const formatTime = new Date(weather.current.dt)
+            .toLocaleTimeString(timeOptions)
+            .replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, '$1$3');
+          this.time = formatTime;
         }
       }
     }
@@ -49,7 +67,8 @@ export default {
 </script>
 
 <style lang="scss">
-h2 {
+h2,
+p {
   text-transform: capitalize;
 }
 </style>
