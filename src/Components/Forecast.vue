ddd2b188dd3a403f00661367765ae77d4839eb86
Morgan <mlpeck@fullsail.edu>
2021-03-28 14:22:55 -0500

    styling the forecast list

--- src/Components/Forecast.vue
+++ src/Components/Forecast.vue
@@ -1,12 +1,12 @@
 <template>
   <section aria-label="Weekly forecast">
     <div class="card" :class="isVisible ? 'visible' : 'hidden'">
-      <ul class="forecast" v-for="day in forecast" :key="day.dt">
-        <li>
+      <ul class="forecast">
+        <li v-for="day in forecast" :key="day.dt">
           <h2>
             {{
               new Date(day.dt * 1000).toLocaleDateString('en-US', {
-                weekday: 'long'
+                weekday: 'short'
               })
             }}
           </h2>
@@ -46,4 +46,44 @@ export default {
 };
 </script>
 
-<style lang=""></style>
+<style lang="scss">
+@import '../assets/styles/styles.scss';
+.forecast {
+  padding: 0;
+
+  list-style: none;
+
+  @media (min-width: 767px) {
+    display: flex;
+    justify-content: space-around;
+
+    flex-wrap: wrap;
+  }
+
+  li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    padding-right: 2rem;
+
+    border-right: solid 1px $ltgrey;
+
+    &:last-of-type {
+      border-right: 0;
+
+      padding: 0;
+    }
+
+    h2 {
+      margin: 0;
+
+      font-size: 1.5rem;
+    }
+    p {
+      margin: 0;
+    }
+  }
+}
+</style>
